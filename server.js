11b94c9488d210581e3a9b622275d9f5f72be09b
a-kanksha <30915518+a-kanksha@users.noreply.github.com>
2017-08-20 15:27:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var express = require('express'); //to create web server
-var morgan = require('morgan'); //to output logs of server -- req and resp
+var express = require('express'); 
+var morgan = require('morgan'); 
 var path = require('path');
 
 var app = express();
